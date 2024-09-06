fd790bfaf6ed837444f90db818548f10d9c91837
Manuel Nuila <manuel.nuila@cuemby.com>
2024-09-06 14:27:25 -0600

    update provider version

--- version.tf
+++ version.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.9.2"
+  required_version = ">= 1.5.7"
 
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.10.1"
+      version = ">= 2.11.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.26.0"
+      version = ">= 2.23.0"
     }
   }
 }
